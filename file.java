2e355bbc5e79b58fc5816cf6422c23df1f46d69a
root <root@ip-172-31-34-152.eu-north-1.compute.internal>
2024-02-23 07:41:19 +0000

    this is my second commit

--- file.java
+++ file.java
@@ -27,11 +27,6 @@ System.out.println();
 //driver code  
 public static void main(String args[])  
 {  
-Scanner sc = new Scanner(System.in);  
-System.out.print("Enter the value of n: ");  
-int n = sc.nextInt();  
-System.out.println();  
-//function calling  
-printPattern(n);  
 }  
-}  
+} 
+This is my second commit
